2e880f072270bd221a9b8e68ca893aba22704144
Del Piero Flores <95143245+dpflores@users.noreply.github.com>
2023-09-28 14:06:49 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -73,7 +73,8 @@ module.exports = {
     /** To password protect the Node-RED editor and admin API, the following
      * property can be used. See http://nodered.org/docs/security.html for details.
      */
-
+    
+    /*
     adminAuth: {
         "type": "credentials",
         "users": [
@@ -84,7 +85,8 @@ module.exports = {
             }
         ]
     },
-
+    */
+    
     /** The following property can be used to enable HTTPS
      * This property can be either an object, containing both a (private) key
      * and a (public) certificate, or a function that returns such an object.
